1ab08a766ee418dea533a7d3018657de176425d5
Jakub Sarnowski <jakub.sarnowski@st-yl.com>
2020-02-25 17:07:17 +0100

    feat: use git projects instead of normal projects

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const utils = require('./lib/utils');
   const matchedProjects = utils.inputMatchesData(
     projects,
     alfy.input,
-    ['name', 'group'],
+    ['name'],
   )
     .sort((a, b) => a.name.localeCompare(b.name))
     .map(project => ({
       title: utils.getTitle(project),
       subtitle: utils.getSubtitle(project),
-      icon: utils.getIcon(project),
+      // icon: utils.getIcon(project),
       arg: utils.getArgument(project),
       valid: true,
       text: {
